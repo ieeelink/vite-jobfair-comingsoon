8fe8b9efbb5375d9da96e284cc12561a0376c313
arjunindia <arjun8107@gmail.com>
2023-02-26 18:41:12 +0530

    Added features(in progress + some refractoring)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 // Import our custom CSS
 import "../scss/styles.scss";
+import VanillaTilt from "vanilla-tilt";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+VanillaTilt.init(document.querySelectorAll(".card"), {
+  max: 2,
+  speed: 10,
+  glare: true,
+  "max-glare": 0.5,
+});
