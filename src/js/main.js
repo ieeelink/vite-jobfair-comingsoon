a4aa4d71afe37083fbe8979b4c0389288fbd3666
arjunindia <arjun8107@gmail.com>
2023-02-28 00:53:12 +0530

    Complete change in themes : Changed the body section significantly

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ import VanillaTilt from "vanilla-tilt";
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
 
-VanillaTilt.init(document.querySelectorAll(".card"), {
+VanillaTilt.init(document.querySelectorAll(".card,.timeline_content"), {
   max: 2,
   speed: 10,
   glare: true,
